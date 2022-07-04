9663e73b53ca70b6529365af6dad90f88ccec4d7
alinakaraca <alinakaraca.07@gmail.com>
2022-07-04 21:53:21 +0300

    Öğrenci Classı ikinci işlemler yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,5 +1,7 @@
 public class Ogrenci {
     public static void main(String[] args) {
-        System.out.println("merhaba dunya");
+
+        System.out.println("merhaba dunya2");
+        System.out.println("merhaba dunya3");
     }
 }
